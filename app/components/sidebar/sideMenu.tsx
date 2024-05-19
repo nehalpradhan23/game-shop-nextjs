1c2bc0dc93089b3cf9ba6998e0b29d10d62137f9
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-05-20 02:38:29 +0530

    first commit

---
+++ app/components/sidebar/sideMenu.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SideMenu = () => {
+  return <div>
+    <a href="/">
+      
+      <span className="">Play</span>
+    </a>
+  </div>;
+};
+
+export default SideMenu;
